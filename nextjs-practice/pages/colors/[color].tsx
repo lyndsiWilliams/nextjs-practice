ec4bfdec33f886a7db05a91a203f27a14f78d531
Lyndsi Kay Williams <kcatgirl@gmail.com>
2023-05-29 17:43:17 -0500

    Add notes about rendering types

--- nextjs-practice/pages/colors/[color].tsx
+++ nextjs-practice/pages/colors/[color].tsx
@@ -17,7 +17,13 @@ export default function Color({
   );
 }
 
-/* --- getServerSideProps will fetch data at every request --- */
+/* --- getServerSideProps will fetch data at every request
+  Server Side Rendering (SSR)
+  SSR pros: Ideal when data is changing constantly, end user will always have the latest data
+  SSR cons: Slower, inefficient data caching. Need to have a server to handle all requests
+    vs. caching everything on a global CDN (content delivery network)
+  Use case example: Ebay
+*/
 export async function getServerSideProps({
   params,
 }: {
@@ -29,7 +35,15 @@ export async function getServerSideProps({
   return { props: { colorData: data } };
 }
 
-/* --- getStaticProps and getStaticPaths will fetch data at build time --- */
+/* --- getStaticProps and getStaticPaths will fetch data at build time (prerendering)
+  Static Site Generation (SSG)
+  SSG Pros: End user gets content quicker, code can be crawled by search bots
+  SSG Cons: Data may become stale, hard to scale many pages (prerender all pages at once can get slow)
+  Use case example: A blog
+
+  --- adding revalidate to the getStaticProps return will fetch data at build time
+    and refresh every `revalidate` seconds - Incremental Statis Generation (ISR)
+*/
 // export async function getStaticProps({
 //   params,
 // }: {
@@ -38,7 +52,10 @@ export async function getServerSideProps({
 //   const request = await fetch(`http://localhost:3000/${params.color}.json`);
 //   const data = await request.json();
 
-//   return { props: { colorData: data } };
+//   return {
+//     props: { colorData: data },
+//     // revalidate: 30 // This would make the app refetch data every 30 seconds, making this ISR
+//   };
 // }
 
 // export async function getStaticPaths() {
